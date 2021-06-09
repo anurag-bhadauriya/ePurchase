2fd4e77d5af24200828fd57f9c5c9f314b98645d
anurag bhadauriya <63424178+anurag-bhadauriya@users.noreply.github.com>
2021-06-09 13:00:22 +0530

    layout created for UI

--- ePurchase-ui/src/app/app-routing.module.ts
+++ ePurchase-ui/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m=> m.AuthModule)
+  },
+  {
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then( m=> m.ProductModule)
+  },
+  {
+    path: 'order',
+    loadChildren: () => import('./order/order.module').then( m=> m.OrderModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then( m=> m.CartModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
